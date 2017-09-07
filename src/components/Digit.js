199c5ab92b550e1b5e93197ebaefffd0329bfcac
Nof Guttman <nofguttman@gmail.com>
2017-09-07 18:28:37 +0300

    a little progress. not much...

---
+++ src/components/Digit.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Digit = ({value, style}) => {
+  return(
+    <div className={style}></div>
+  );
+}
+
+export default Digit;
--- src/components/DigitSelect.js
+++ src/components/DigitSelect.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import Digit from './Digit';
 
 const DigitSelect = () => {
   return(
     <div>
-      <select>
-        <option>0</option>
-        <option>1</option>
-        <option>2</option>
-      </select>
+      <h1>Dude</h1>
+      <Digit value={0} style={"red digit"} />
+      <Digit value={1} style={"yellow digit"} />
+      <Digit value={2} style={"green digit"} />
+      <Digit value={3} style={"blue digit"} />
+      <Digit value={4} style={"purple digit"} />
+      <Digit value={5} style={"orange digit"} />
     </div>
   );
 }
